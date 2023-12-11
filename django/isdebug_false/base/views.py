7e95686a9525d7af82be66e2eae961eb5106baf7
pembattech <pembat.tech@gmail.com>
2023-12-11 10:28:34 +0545

    Add isdebug_false django project.

---
+++ django/isdebug_false/base/views.py
@@ -0,0 +1,6 @@
+# myapp/views.py
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
---
+++ django/isdebug_false/readme.md
@@ -0,0 +1,3 @@
+# isdebug_false
+
+Fixed issue with static files not loading when DEBUG = False.
